5c6700a20fa249fe56d9fdb54dcaf3d29fc4075a
tk-climb <hank9323@gmail.com>
2023-01-17 09:43:56 +0900

    bean 못찾는 오류 확인 - 하태경

--- PandaClimb/src/main/java/com/study/panda/PandaClimbApplication.java
+++ PandaClimb/src/main/java/com/study/panda/PandaClimbApplication.java
@@ -2,6 +2,7 @@ package com.study.panda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class PandaClimbApplication {
--- PandaClimb/src/main/java/com/study/panda/shop/controller/ShopController.java
+++ PandaClimb/src/main/java/com/study/panda/shop/controller/ShopController.java
@@ -11,31 +11,32 @@ import com.study.panda.shop.dao.ShopDao;
 @RequestMapping(value = "/shop")
 public class ShopController {
 
-	@Autowired
-	private ShopDao shopDao;
-    
-	  @GetMapping(value="/shop.do") public String listView() throws Exception{
-		  
-		  return "/shop"; }
+		@Autowired ShopDao shopdao;
+		
+		@GetMapping(value="/shop.do") public String listView() throws Exception{
+  
+			return "/shop/shop";
+			
+		}
 
-    
-	/*
-	 * @GetMapping(value="/list.do") public String listView(Model model, NoticeDto
-	 * noticeDto) throws Exception{ List<NoticeDto> AllList =
-	 * homeDao.listView(noticeDto); model.addAttribute("AllList",AllList);
-	 * model.addAttribute("greet","greeting");
-	 * System.out.println("@@@@@@@ 최종값은? @@@@"+AllList); return "/home"; }
-	 */
-    
-	/*
-	 * @GetMapping(value="/insert") public String insert() { return "notice/insert";
-	 * }
-	 * 
-	 * @PostMapping(value="/insert") public String insert(HttpSession
-	 * session,@ModelAttribute NoticeDto noticeDto, RedirectAttributes attr) {
-	 * System.out.println(noticeDto); homeDao.insert(noticeDto);
-	 * 
-	 * return "redirect:list"; }
-	 */
+
+/*
+ * @GetMapping(value="/list.do") public String listView(Model model, NoticeDto
+ * noticeDto) throws Exception{ List<NoticeDto> AllList =
+ * homeDao.listView(noticeDto); model.addAttribute("AllList",AllList);
+ * model.addAttribute("greet","greeting");
+ * System.out.println("@@@@@@@ 최종값은? @@@@"+AllList); return "/home"; }
+ */
+
+/*
+ * @GetMapping(value="/insert") public String insert() { return "notice/insert";
+ * }
+ * 
+ * @PostMapping(value="/insert") public String insert(HttpSession
+ * session,@ModelAttribute NoticeDto noticeDto, RedirectAttributes attr) {
+ * System.out.println(noticeDto); homeDao.insert(noticeDto);
+ * 
+ * return "redirect:list"; }
+ */
     
 }
