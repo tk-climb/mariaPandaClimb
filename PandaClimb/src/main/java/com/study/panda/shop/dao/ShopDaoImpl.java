a6a132ce2b186bcb8aa0222dfbfbc0a179daa5fa
tk-climb <hank9323@gmail.com>
2023-01-18 09:04:42 +0900

    쇼핑 프론트 완성 - 하태경

--- PandaClimb/src/main/java/com/study/panda/shop/dao/ShopDaoImpl.java
+++ PandaClimb/src/main/java/com/study/panda/shop/dao/ShopDaoImpl.java
@@ -1,5 +1,15 @@
 package com.study.panda.shop.dao;
 
-public class ShopDaoImpl {
+import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ShopDaoImpl implements ShopDao {
+	
+	   @Autowired
+	   private SqlSession sqlSession;
+	   
 }
