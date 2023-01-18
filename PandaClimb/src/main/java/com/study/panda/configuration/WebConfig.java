6230cdab443a8b16111935c005468198f51f688b
tk-climb <hank9323@gmail.com>
2023-01-18 19:13:03 +0900

    test

---
+++ PandaClimb/src/main/java/com/study/panda/configuration/WebConfig.java
@@ -0,0 +1,17 @@
+package com.study.panda.configuration;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+	
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("C://img/")
+				.addResourceLocations("C://img/");
+	}
+	
+}
